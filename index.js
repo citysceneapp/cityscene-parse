238dcbe328e3d6f227565e01501a2bdd4a60cd45
Nicole Serras <citysceneapp@gmail.com>
2018-12-08 18:03:46 -0500

    Added mailgun settings

--- index.js
+++ index.js
@@ -8,18 +8,34 @@ var path = require('path');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'myAppId',
+    masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ['Posts', 'Comments'] // List of classes to support for query subscriptions
+    },
+    verifyUserEmails: true,
+    publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+    appName: process.env.APP_NAME || 'CityScene',
+    emailAdapter: {
+        module: 'parse-server-simple-mailgun-adapter',
+        options: {
+            fromAddress:
+                process.env.EMAIL_FROM || 'no-reply.citysceneapp@gmail.com',
+            domain:
+                process.env.MAILGUN_DOMAIN ||
+                'sandbox43c4b5db6b9247bea01631f7cd1598a5.mailgun.org',
+            apiKey:
+                process.env.MAILGUN_API_KEY ||
+                'de97c37bdaf719dc4355f070e3175abf-52cbfb43-f147c13b'
+        }
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -36,13 +52,15 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+    res.status(200).send(
+        'I dream of being a website.  Please star the parse-server repo on GitHub!'
+    );
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+    res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
